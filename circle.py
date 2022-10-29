4ba10727af0545443ee312b4461cfffb518c1bee
Anita Justin <116017002+kurnu@users.noreply.github.com>
2022-10-29 23:00:13 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -5,14 +5,4 @@ p=2*pi*a
 a=pi*a*a
 print(f"the perimeter is",p,"\n the area is ",a,"\n")
 
-print("to convert degree to farenheit")
-t=float(input("enter the temperature in degree"))
-f=(t*1.8)+32
-print(f"the temperature in farenheit is:",f,"\n")
-
-print("to swap numbers")
-a=input("enter a")
-b=input("enter b")
-a,b=b,a
-print(f"the number after swapping;a=",a,"b=",b)
 
