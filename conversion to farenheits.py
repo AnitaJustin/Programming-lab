d38eb405dfc31aba63fc9dfeb23f5a157c17d84f
Anita Justin <116017002+kurnu@users.noreply.github.com>
2022-10-29 23:01:50 +0530

    Add files via upload

---
+++ conversion to farenheits.py
@@ -0,0 +1,4 @@
+print("to convert degree to farenheit")
+t=float(input("enter the temperature in degree"))
+f=(t*1.8)+32
+print(f"the temperature in farenheit is:",f,"\n")
---
+++ swapping numbers.py
@@ -0,0 +1,5 @@
+print("to swap numbers")
+a=input("enter a")
+b=input("enter b")
+a,b=b,a
+print(f"the number after swapping;a=",a,"b=",b)
